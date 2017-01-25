690e10865cf8a68c285bfb5beb66cef57ac4e0b0
zhangwei <719048757@qq.com>
2017-01-26 00:17:49 +0800

    code

--- app/app.js
+++ app/app.js
@@ -5,15 +5,27 @@ angular.module('eshopCms', [
   'ui.router'
 ])
 .config(function($stateProvider) {
+  //设置首页
+  var indexState = {
+    name: 'index',
+    url: '',
+    controller: 'statisticsController',
+    templateUrl: 'statistics/statistics.html'
+  }
+
   var productsListState = {
     name: 'productsList',
     url: '/products/list',
     controller: 'productsListController',
-    template: 'product-list.html'
+    templateUrl: '/product/product-list.html'
   }
 
+  $stateProvider.state(indexState);
   $stateProvider.state(productsListState);
 })
+.controller('statisticsController', [function($scope) {
+  console.log('statisticsController');
+}])
 .controller('applicationController', [function($scope) {
   console.log('applicationController');
 }])
